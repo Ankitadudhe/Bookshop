6f395bec917106eef7f20eceea7991318f425243
Ankitadudhe <ankitadudhe95@gmail.com>
2021-01-30 11:32:07 +0530

    modified adventure

--- Bookshop/src/component/pages/adventure/index.js
+++ Bookshop/src/component/pages/adventure/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import {Card} from '@material-ui/core';
+import {Card,Typography} from '@material-ui/core';
 import AddToCart from '../../atom/addToCart';
 import * as actions from "../../../redux/actions/addTocart";
 import './adventure.css';
 import {connect} from "react-redux";
 import Header from '../../organisms/header';
+import Image from "material-ui-image";
 
 class Adventure extends Component {
 
@@ -31,26 +32,28 @@ class Adventure extends Component {
     render() {
         let productsall=this.props.product;
         console.log("product lenght",productsall);
-        // Axios.post("http://localhost:5000/adventure").then((data)=>{
-        //     console.log("data from database",data);
-        // })
+      
         return (
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: '20px' ,justifyContent:'space-between',flex:3}}>
+            <div classname="adventuresec" style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', marginTop: '20px' ,justifyContent:'space-between',flex:3}}>
                 {
                    this.state.books.map((bookdata)=>(
                        <Card>
-                           <div>
-                               <img src={bookdata.image} />
-                           </div>
-                           <div>
+                           <Image
+                             src={bookdata.image}
+                              style={{ padding: 0, height: 200, textAlign: "center" }}
+                              imageStyle={{ width: 350, position: "static" }}
+                              disableSpinner
+                           />
+                           <Typography gutterBottom variant="h6" component="p" color="primary">
                                Author:{bookdata.author}
-                           </div>
-                           <div>
-                               Title:{bookdata.title}
-                           </div>
-                           <div>
-                               Price:Rs{bookdata.price}
-                           </div>
+                            </Typography>
+                           <Typography gutterBottom variant="h6" component="p" color="primary">
+
+                           Title:{bookdata.title}
+                         </Typography>
+                            <Typography style={{ fontWeight: 500 }} color="secondary" component="p">
+                               Price:Rs {bookdata.price}
+                              </Typography>
                            <AddToCart 
                            products={this.props.product}
                            />
